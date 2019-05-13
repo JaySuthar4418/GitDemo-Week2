31b69896a1faa36c0bd0c51ec380df3777293fa3
Jay <Jay@10.16.25.213>
2019-05-13 12:46:02 -0400

    This is a trial repository for Week 2.

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author Jay
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args, int i)
+    {
+        int [] a = new int[5];
+        a[0]= '1';
+        a[1]= '3';
+        a[2]= '4';
+        a[3]= '2';
+        a[4]= '5';
+        
+        for (i=0;i>5;i++)
+        {
+            system.out.print (a + " ");
+        }
+        
+    }
+    
+}
